4475ee87ec85428fc6a96ede9312cd380156fbeb
majesiu <pawel.majewski@mail.com>
2016-03-30 21:46:00 +0200

    Icht worken

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,9 +6,11 @@ import java.io._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
-object Main extends App {
+object main extends App {
 
   case class Team(name: List[String])
+  case class Avalaible(GAMEID: String, PT_AVAILABLE: Int)
+  case class Datarow(name: String, headers: List[String],rowSet: List[Avalaible])
 
   override def main(args: Array[String]): Unit = {
 
@@ -22,23 +24,39 @@ object Main extends App {
           .asString.toString.split('(')
 
     val d = d2.drop(1).mkString("(").split('}')
-    val data = d.take(d.size-1).mkString("}").concat("}")
+    val data = d.take(d.length-1).mkString("}").concat("}")
 
     val js = Json.parse(data)
 
     val datasets = (js \ "resultSets").get
-    val games =  datasets(1)
+    val games =  datasets(6).get
+    val name = (games \ "headers").get
 
+    println(Json.prettyPrint(name))
 
-    val file = new File("C:\\Users\\Majesiu\\Desktop\\datasets.json")
+    val file = new File("I:\\Users\\majesiu\\Desktop\\datasets.json")
     val bw = new BufferedWriter(new FileWriter(file))
     bw.write(Json.prettyPrint(datasets))
     bw.close()
 
+    implicit val avalaibleReads: Reads[Avalaible] = (
+        JsPath(0).read[String] and
+        JsPath(1).read[Int]
+      )(Avalaible.apply _)
 
-    val file2 = new File("C:\\Users\\Majesiu\\Desktop\\games.json")
+    implicit val datarowReads: Reads[Datarow] = (
+        (JsPath  \ "name").read[String] and
+        (JsPath  \ "headers").read[List[String]] and
+        (JsPath  \ "rowSet").read[List[Avalaible]]
+      )(Datarow.apply _)
+
+    val datarowResult: JsResult[Datarow] = games.validate[Datarow]
+
+    println(datarowResult.get)
+
+    val file2 = new File("I:\\Users\\majesiu\\Desktop\\games.json")
     val bw2 = new BufferedWriter(new FileWriter(file2))
-    bw2.write(Json.prettyPrint(games.get))
+    bw2.write(Json.prettyPrint(games))
     bw2.close()
   }
 
